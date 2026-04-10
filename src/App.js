35a0f8d887eced81c33cfc56269ea14645085d4c
adarsh985 <2024.adarshs@isu.ac.in>
2026-04-10 12:08:41 +0530

    feat: update App component link text to 'Learn Devops with react'

--- src/App.js
+++ src/App.js
@@ -15,7 +15,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+
+          Learn Devops with react
         </a>
       </header>
     </div>
